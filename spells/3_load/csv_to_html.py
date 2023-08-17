fdfa62ad4ceef527a3e5f1c6d6128abcd059cb00
0foo <0foo@0foo.dev>
2023-08-17 15:09:18 -0600

    upd

--- spells/3_load/csv_to_html.py
+++ spells/3_load/csv_to_html.py
@@ -1,3 +1,17 @@
 import pandas as pd
+import csv, json
+
+def csv_to_json(in_csv_file, out_json_file):
+    out_json_list = []
+    with open(in_csv_file) as csv_file:
+        csvReader = csv.DictReader(csv_file)
+        for row in csvReader:
+            out_json_list.append(row)
+    with open(out_json_file, "w") as json_file:
+        json_file.write(json.dumps(out_json_list))
+
+
 a = pd.read_csv("../format/spells.csv")
 a.to_html("../format/spells.html", table_id="spells_table")
+csv_to_json("../format/spells.csv", "../format/spells.json")
+# a.to_json("../format/spells.json", orient="records")
